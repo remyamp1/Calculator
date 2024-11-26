4e7d5ed0d9d2fea89b8cf9ddc31a6952ddb239bf
remyamp <remyamp774@gmail.com>
2024-11-26 13:28:45 +0530

    26-11-24

--- lib/Calculator.dart
+++ lib/Calculator.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:math_expressions/math_expressions.dart';
 
 class CalculatorApp extends StatefulWidget {
   const CalculatorApp({super.key});
@@ -19,7 +20,7 @@ class _CalculatorAppState extends State<CalculatorApp> {
     "9",
     "8",
     "7",
-    "X",
+    "x",
     "6",
     "5",
     "4",
@@ -42,6 +43,9 @@ class _CalculatorAppState extends State<CalculatorApp> {
         padding: const EdgeInsets.all(8.0),
         child: Column(
           children: [
+            SizedBox(
+              height: 100,
+            ),
             Text(UserQuestion),
             Row(
               mainAxisAlignment: MainAxisAlignment.end,
@@ -56,27 +60,126 @@ class _CalculatorAppState extends State<CalculatorApp> {
               height: 30,
             ),
             Expanded(
-              child: GridView.builder(
-                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                      mainAxisSpacing: 7,
-                      crossAxisSpacing: 7,
-                      crossAxisCount: 4),
-                  itemCount: button.length,
-                  itemBuilder: (context, index) {
-                    return Container(
-                      height: 30,
-                      width: 30,
-                      decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(10),
-                          color: Colors.pinkAccent),
-                      child: Center(child: Text(button[index])),
-                      
-                    );
-                  }),
-            )
+                child: GridView.builder(
+                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                        mainAxisSpacing: 7,
+                        crossAxisSpacing: 7,
+                        crossAxisCount: 4),
+                    itemCount: button.length,
+                    itemBuilder: (context, index) {
+                      if (index == 0) {
+                        return GestureDetector(
+                          onTap: () {
+                            setState(() {
+                              UserAnswer = '';
+                              UserQuestion = '';
+                            });
+                          },
+                          child: Container(
+                            height: 30,
+                            width: 30,
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(10),
+                                color:
+                                    const Color.fromARGB(255, 179, 250, 206)),
+                            child: Center(
+                              child: Text(button[index]),
+                            ),
+                          ),
+                        );
+                      } else if (index == 1) {
+                        return GestureDetector(
+                          onTap: () {
+                            setState(() {
+                              UserQuestion = UserQuestion.substring(
+                                  0, UserQuestion.length - 1);
+                            });
+                          },
+                          child: Container(
+                            height: 30,
+                            width: 30,
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(10),
+                                color: Colors.red),
+                            child: Center(child: Text(button[index])),
+                          ),
+                        );
+                      } else if (index == button.length - 1) {
+                        return GestureDetector(
+                          onTap: () {
+                            setState(() {
+                              equalPressed();
+                            });
+                          },
+                          child: Container(
+                            height: 30,
+                            width: 30,
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(10),
+                                color: Colors.purple),
+                            child: Center(
+                                child: Text(
+                              button[index],
+                              style: TextStyle(
+                                  fontSize: 25, fontWeight: FontWeight.bold),
+                            )),
+                          ),
+                        );
+                      } else {
+                        return GestureDetector(
+                          onTap: () {
+                            setState(() {
+                              UserQuestion += button[index];
+                            });
+                          },
+                          child: Container(
+                            height: 30,
+                            width: 30,
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(10),
+                                color: isOperator(button[index])
+                                    ? const Color.fromARGB(255, 199, 178, 236)
+                                    : const Color.fromARGB(255, 168, 128, 238)),
+                            child: Center(
+                                child: Text(
+                              (button[index]),
+                              style: TextStyle(color: Colors.deepPurple),
+                            )),
+                          ),
+                        );
+                      }
+                      return Container(
+                        height: 30,
+                        width: 30,
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10),
+                            color: const Color.fromARGB(255, 248, 157, 225)),
+                        child: Center(child: Text(button[index])),
+                      );
+                    }))
           ],
         ),
       ),
     );
+  }
+
+  bool isOperator(String x) {
+    if (x == '%' || x == '/' || x == 'x' || x == '+' || x == '-' || x == '=') {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  void equalPressed() {
+    String finalQuestion = UserQuestion;
+    finalQuestion = finalQuestion.replaceAll('x', '*');
+
+    Parser p = Parser();
+    Expression exp = p.parse(finalQuestion);
+    ContextModel cm = ContextModel();
+    double eval = exp.evaluate(EvaluationType.REAL, cm);
+
+    UserAnswer = eval.toString();
   }
 }
